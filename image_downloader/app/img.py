737f6f366133d2f6fe8c9af2aa4d30fb6b385d01
Peter Petrov <97050310+pjpetrov@users.noreply.github.com>
2022-03-29 23:43:00 +0300

    Leave a TODO to extract some hardcoded values.

--- image_downloader/app/img.py
+++ image_downloader/app/img.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 cache = redis.Redis(host='redis', port=6379)
 
+# TODO extract this IPs from the file.
 picUrls = {'photo_garage12' : 'https://192.168.2.248/cgi-bin/currentpic.cgi',
             'photo_garage13' : 'https://192.168.2.249/cgi-bin/currentpic.cgi',
             'photo_office4' : 'https://192.168.100.201/cgi-bin/currentpic.cgi'}
@@ -25,7 +26,8 @@ def update(msg):
             valStr = val.decode("utf-8")
             resultTarget = valStr.split(',')[1]
             logging.info("loading photo from: " + picUrls[key])
-            content = requests.get(picUrls[key], verify=False, auth=('root','ismart12')).content
+            # TODO Extract the authentication from this file.
+            content = requests.get(picUrls[key], verify=False, auth=('root','')).content
             logging.info("sending photo to: (" + resultTarget + ')')
             cache.setex(resultTarget, datetime.timedelta(seconds=10), value=content)
 
